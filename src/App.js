953e3b00fad8f01da2d7ad110efa4f98c89b5301
Chris Couzens <ccouzens@ukcloud.com>
2019-01-15 17:09:03 +0000

    Add prettier to try and keep the code looking consistent
    It likes semicolons...

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person'
+import Person from './Person/Person';
 
 class App extends Component {
   state = {
@@ -10,30 +10,32 @@ class App extends Component {
       { name: 'Stepahnie', age: 26 }
     ],
     showPersons: false
-  }
+  };
 
-  switchNameHandler = (newName) => {
-    this.setState({persons: [
-      { name: newName, age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Stepahnie', age: 27 }
-    ]})
-  }
+  switchNameHandler = newName => {
+    this.setState({
+      persons: [
+        { name: newName, age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stepahnie', age: 27 }
+      ]
+    });
+  };
 
-  nameChangedHandler = (event) => {
+  nameChangedHandler = event => {
     this.setState({
       persons: [
         { name: 'Max', age: 28 },
         { name: event.target.value, age: 29 },
         { name: 'Stepahnie', age: 26 }
       ]
-    })
-  }
+    });
+  };
 
   togglePersonsHandler = () => {
-    const doesShow = this.state.showPersons
-    this.setState({showPersons: !doesShow})
-  }
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  };
 
   render() {
     const style = {
@@ -42,28 +44,35 @@ class App extends Component {
       border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer'
-    }
+    };
 
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button
-          style={style}
-          onClick={this.togglePersonsHandler}>Toggle Persons</button>
-          { this.state.showPersons ? <div>
+        <button style={style} onClick={this.togglePersonsHandler}>
+          Toggle Persons
+        </button>
+        {this.state.showPersons ? (
+          <div>
             <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age} />
+              name={this.state.persons[0].name}
+              age={this.state.persons[0].age}
+            />
             <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this, 'Max')}
-            changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
+              name={this.state.persons[1].name}
+              age={this.state.persons[1].age}
+              click={this.switchNameHandler.bind(this, 'Max')}
+              changed={this.nameChangedHandler}
+            >
+              My Hobbies: Racing
+            </Person>
             <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age} />
-          </div> : null }
+              name={this.state.persons[2].name}
+              age={this.state.persons[2].age}
+            />
+          </div>
+        ) : null}
       </div>
     );
   }
