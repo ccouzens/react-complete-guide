34b111ad2d4bed0b9e8cd91f71220db9009a7716
Chris Couzens <ccouzens@ukcloud.com>
2019-01-17 10:07:49 +0000

    Use key prop on list to help react

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Stepahnie', age: 26 }
+      { id: 'asdf', name: 'Max', age: 28 },
+      { id: 'qwer', name: 'Manu', age: 29 },
+      { id: 'zxcv', name: 'Stepahnie', age: 26 }
     ],
     showPersons: false
   };
@@ -52,6 +52,7 @@ class App extends Component {
               click={this.deletePersonHandler.bind(this, index)}
               name={person.name}
               age={person.age}
+              key={person.id}
             />
           ))}
         </div>
